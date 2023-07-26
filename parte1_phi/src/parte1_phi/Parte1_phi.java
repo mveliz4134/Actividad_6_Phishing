6cc248a84e85669bc4588c8b1e32fa7061a4003d
mveliz4134 <136132321+mveliz4134@users.noreply.github.com>
2023-07-25 22:17:21 -0500

    Add files via upload

---
+++ parte1_phi/src/parte1_phi/Parte1_phi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte1_phi;
+
+/**
+ *
+ * @author Clarens
+ */
+public class Parte1_phi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        //Se hace la primera Parte 
+        
+          String[] lista = {
+                "Urgente", "Iniciar sesion", "Premio", "Gana dinero rapido", "Ganador",
+                "Gratis", "Cuenta bloqueada", "Caducidad de cuenta", "Confirmacion requerida", "Actualizar informacion",
+                "Confirmar datos", "Contraseña", "Verificacion de cuenta", "Actualizacion urgente", "Oferta exclusiva", 
+                "Problema de seguridad", "Suspension de cuenta", "Promocion especial", "Actualice informacion", 
+                "Problema de seguridad", "Permio", "Restablecer cuenta", "Problem de inicio de sesion", "Oferta limitada",
+                "Recuperar acceso", "Advertencia de seguridad", "Confirma identidad", "Promocion increible", 
+                "Actualizar ahora", "Cierra de cuenta"        
+               
+        };
+        int[] arreglo = {1, 3, 2, 2, 3, 1, 1, 3, 3, 3, 2, 1, 1, 2, 2, 3, 2, 1, 2, 2, 1, 3, 3, 3, 2, 1, 1, 2, 1, 2};
+
+    }
+    
+}
